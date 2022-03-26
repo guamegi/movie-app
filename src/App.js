82aac1e4b2159bed0877de149b92c898ecd620d0
Minha Kwon <namisoft@naver.com>
2022-03-26 23:33:28 +0900

    genres 추가

--- src/App.js
+++ src/App.js
@@ -27,6 +27,11 @@ function App() {
               <img src={movie.medium_cover_image} />
               <h2>{movie.title}</h2>
               <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
             </div>
           ))}
         </div>
