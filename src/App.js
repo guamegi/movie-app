3c2ed08effeae6b961431bea9c27a46c37f49780
Minha Kwon <namisoft@naver.com>
2022-03-27 00:46:33 +0900

    routing 처리

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
-import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
-  }, []);
-  console.log(movies);
-
   return (
-    <div>
-      {loading ? (
-        <h1>loading</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-            />
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/movie" element={<Detail />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
